3df51182ae3606df07a8412b6326ab2789ab9230
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-05-04 15:57:09 +0200

    Add invalidation of rectangles to redraw
    This commit adds the ei_app_invalidate_rectangle function implementation
    and stores all the rectangles to redraw. When redrawing, each rectangle
    is used as a clipper (to avoid redrawing outside of the rectangle)
    Invalidated rectangles have no optimization yet. Some will be added soon.
    This commit also adds a debug flag to print some information

--- implem/src/ei_application.c
+++ implem/src/ei_application.c
@@ -6,6 +6,7 @@
 #include "../api/ei_geometrymanager.h"
 #include "../api/ei_event.h"
 #include "../api/ei_types.h"
+#include "../api/ei_utils.h"
 #include "../implem/headers/ei_button.h"
 #include "../implem/headers/ei_implementation.h"
 #include "../implem/headers/ei_widgetclass_ext.h"
@@ -14,11 +15,13 @@
 #include "../implem/headers/ei_frame.h"
 #include "../implem/headers/ei_internal_callbacks.h"
 #include "../implem/headers/ei_widget_ext.h"
+#include "../implem/headers/ei_application_ext.h"
 
 ei_widget_t root = NULL;
 ei_surface_t window_surface = NULL;
 ei_surface_t offscreen_picking = NULL;
 bool main_loop_running = true;
+ei_linked_rect_t *invalid_rects = NULL;
 
 void ei_app_create(ei_size_t main_window_size, bool fullscreen)
 {
@@ -40,47 +43,69 @@ void ei_app_create(ei_size_t main_window_size, bool fullscreen)
 
 void ei_app_run(void)
 {
+    // Only used for debugging
+    int main_loop_count = 0;
+
     ei_event_t event;
+
+    // Invalidate the root widget once to draw the whole screen
+    ei_app_invalidate_rect(&ei_app_root_widget()->screen_location);
+
+    ei_widget_t current = ei_app_root_widget();
+    while (true)
+    {
+        // Recompute the geometry of the widget
+        if (current->geom_params != NULL)
+        {
+            current->geom_params->manager->runfunc(current);
+        }
+
+        if (current->next_sibling != NULL)
+        {
+            current = current->next_sibling;
+        }
+        else if (current->children_head != NULL)
+        {
+            current = current->children_head;
+        }
+        else
+        {
+            break;
+        }
+    }
 
     while (main_loop_running)
     {
-        ei_widget_t current = ei_app_root_widget();
+        DEBUG ? printf("Running main loop %d\n", main_loop_count++) : 0;
 
-        if (event.type != ei_ev_mouse_move)
+        hw_surface_lock(window_surface);
+        hw_surface_lock(offscreen_picking);
+
+        // Redraw all the widgets but use each invalid_rect as a clipper so that we don't redraw the whole screen every time
+        if (invalid_rects != NULL)
         {
-            hw_surface_lock(window_surface);
-            hw_surface_lock(offscreen_picking);
-
-            while (true)
+            for (ei_linked_rect_t *invalid_rect = invalid_rects; invalid_rect != NULL; invalid_rect = invalid_rect->next)
             {
-                // Recomputer the geometry of the widget
-                if (current->geom_params != NULL)
-                {
-                    current->geom_params->manager->runfunc(current);
-                }
+                DEBUG ? printf("Drawing invalid rect %d %d %d %d\n", invalid_rect->rect.top_left.x, invalid_rect->rect.top_left.y, invalid_rect->rect.size.width, invalid_rect->rect.size.height) : 0;
 
-                if (current->next_sibling != NULL)
-                {
-                    current = current->next_sibling;
-                }
-                else if (current->children_head != NULL)
-                {
-                    current = current->children_head;
-                }
-                else
-                {
-                    break;
-                }
+                // Redraw the root widget
+                ei_app_root_widget()->wclass->drawfunc(ei_app_root_widget(), window_surface, offscreen_picking, &invalid_rect->rect);
             }
-
-            // Redraw the root widget
-            ei_app_root_widget()->wclass->drawfunc(ei_app_root_widget(), window_surface, offscreen_picking, NULL);
-
-            hw_surface_unlock(window_surface);
-            hw_surface_unlock(offscreen_picking);
-
-            hw_surface_update_rects(window_surface, NULL);
         }
+
+        hw_surface_unlock(window_surface);
+        hw_surface_unlock(offscreen_picking);
+
+        hw_surface_update_rects(window_surface, invalid_rects);
+
+        // Free the invalid_rects linked list
+        ei_linked_rect_t *next_invalid_rect;
+        for (ei_linked_rect_t *invalid_rect = invalid_rects; invalid_rect != NULL; invalid_rect = next_invalid_rect)
+        {
+            next_invalid_rect = invalid_rect->next;
+            free(invalid_rect);
+        }
+        invalid_rects = NULL;
 
         hw_event_wait_next(&event);
 
@@ -97,8 +122,23 @@ void ei_app_free(void)
 
 void ei_app_invalidate_rect(const ei_rect_t *rect)
 {
-    // TODO
-    printf("Not implemented yet!");
+    DEBUG ? printf("Invalidating rect %d %d %d %d\n", rect->top_left.x, rect->top_left.y, rect->size.width, rect->size.height) : 0;
+
+    // If the linked list is empty, add the first element
+    if (invalid_rects == NULL)
+    {
+        invalid_rects = malloc(sizeof(ei_linked_rect_t));
+        invalid_rects->rect = *rect;
+        invalid_rects->next = NULL;
+    }
+    // Otherwise, insert the rectangle at the head of the linked list so that we don't have to traverse the whole list
+    else
+    {
+        ei_linked_rect_t *invalid_rect = malloc(sizeof(ei_linked_rect_t));
+        invalid_rect->rect = *rect;
+        invalid_rect->next = invalid_rects;
+        invalid_rects = invalid_rect;
+    }
 }
 
 void ei_app_quit_request(void)
