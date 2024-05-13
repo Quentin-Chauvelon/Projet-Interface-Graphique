4650038cc077740a7d05d851d5552fd2ec530902
badianes <seydina.badiane@grenoble-inp.org>
2024-05-13 18:13:24 +0200

    ei_implem_map_rgba corrigée

--- implem/src/ei_implementation.c
+++ implem/src/ei_implementation.c
@@ -37,22 +37,21 @@ void ei_impl_widget_draw_children(ei_widget_t widget, ei_surface_t surface, ei_s
 
 uint32_t ei_impl_map_rgba(ei_surface_t surface, ei_color_t color)
 {
-    uint32_t mapped_color = 0 ;
+    uint32_t converted_color = 0 ;
     int ir, ig , ib, ia ;
-    // get the chanel indices 
-    hw_surface_get_channel_indices(ei_surface_t surface, int* ir, int* ig, int* ib, int* ia);
 
-    mapped_color |= ( (uint32_t)color.red ) << 24;
-    mapped_color |= ( (uint32_t)color.green ) << 16;
-    mapped_color |= ( (uint32_t)color.blue ) << 8;
-    // case where the surface have an alpha chanel
-    if (*ia =! -1)
+    // get the channel indices of the surface 
+    hw_surface_get_channel_indices(ei_surface_t surface, &ir, &ig, &ib, &ia);
+
+    //position each byte R, G, B, A in the right place in the 32-bit integer
+    converted_color |= ( (uint32_t)color.red ) << 32-(ir + 1)*8 ;
+    converted_color |= ( (uint32_t)color.green ) << 32-(ig + 1)*8;
+    converted_color |= ( (uint32_t)color.blue ) << 32-(ib + 1)*8;
+    // case where the surface has an alpha chanel
+    if (ia =! -1)
     {
-        mapped_color |= (uint32_t)color.alpha ;
+        converted_color |= ( (uint32_t)color.alpha ) << 32-(ia + 1)*8 ;
     }
-    //store this pixel in the surface with good chanel indices
-    //to complete...
-
     
-    return mapped_color;
+    return converted_color;
 }
