53ed176787c1f2211d7f3b530096e8f6e6e59f31
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-05-14 17:52:32 +0200

    Add documentation for the parser

--- implem/headers/parser/ei_parser_option_value_conversion.h
+++ implem/headers/parser/ei_parser_option_value_conversion.h
@@ -11,18 +11,62 @@
 #ifndef EI_PARSER_OPTION_VALUE_CONVERSION_H
 #define EI_PARSER_OPTION_VALUE_CONVERSION_H
 
+/**
+ * @brief   Convert a list of strings ( \ref ei_list_name_t ) to a single string.
+ *
+ * @return  The string converted from the list of strings
+ */
 char *ei_option_value_to_string();
 
+/**
+ * @brief   Convert a list of numbers ( \ref ei_list_number_t ) to a color.
+ *          The list must contain exactly 4 numbers or it will return a default color
+ *
+ * @return  The color converted from the list of numbers
+ */
 ei_color_t ei_option_value_to_color();
 
+/**
+ * @brief   Convert a list of numbers ( \ref ei_list_number_t ) to a size.
+ *          The list must contain exactly 2 numbers or it will return a default size
+ *
+ * @return  The size converted from the list of numbers
+ */
 ei_size_t ei_option_value_to_size();
 
+/**
+ * @brief   Convert a list of numbers ( \ref ei_list_number_t ) to a rect.
+ *          The list must contain exactly 4 numbers or it will return a default rect
+ *
+ * @return  The rect converted from the list of numbers
+ */
 ei_rect_t ei_option_value_to_rect();
 
+/**
+ * @brief   Convert the given generic pointer to an \ref ei_anchor_t
+ *
+ * @param option_value  The generic pointer to convert to an anchor
+ *
+ * @return  The anchor converted from the generic pointer
+ */
 ei_anchor_t ei_option_value_to_anchor(void *option_value);
 
+/**
+ * @brief   Convert the given generic pointer to an \ref ei_axis_set_t
+ *
+ * @param option_value  The generic pointer to convert to an axis
+ *
+ * @return  The axis converted from the generic pointer
+ */
 ei_axis_set_t ei_option_value_to_axis_set(void *option_value);
 
+/**
+ * @brief   Convert the given generic pointer to an \ref ei_relief_t
+ *
+ * @param option_value  The generic pointer to convert to a relief
+ *
+ * @return  The relief converted from the generic pointer
+ */
 ei_relief_t ei_option_value_to_relief(void *option_value);
 
 #endif
--- implem/headers/parser/ei_parser_types.h
+++ implem/headers/parser/ei_parser_types.h
@@ -18,18 +18,31 @@ extern struct ei_list_number_t *ei_list_number;
 extern struct ei_list_name_t *ei_list_name;
 extern int error;
 
+/**
+ * @brief   A struct to hold a list of numbers which can then be converted
+ *          to other structs such as color, size, rect...
+ */
 typedef struct ei_list_number_t
 {
     double value;
     struct ei_list_number_t *next;
 } ei_list_number_t;
 
+/**
+ * @brief   A struct to hold a list of names which can then be converted
+ *         to a single string
+ */
 typedef struct ei_list_name_t
 {
     char *name;
     struct ei_list_name_t *next;
 } ei_list_name_t;
 
+/**
+ * @brief   A struct to hold a list of widgets which allows to associate
+ *          a name to a widget. Allows mapping from the names in the parsed
+ *          file to the widgets instantiated by the parser (for events for example)
+ */
 typedef struct ei_widgets_list_t
 {
     char *name;
@@ -37,24 +50,52 @@ typedef struct ei_widgets_list_t
     struct ei_widgets_list_t *next;
 } ei_widgets_list_t;
 
+/**
+ * @brief   An enum describing the what the option refers to (configuring
+ *          or placing a widget)
+ */
 enum options_action
 {
     OPTION_CONFIGURE,
     OPTION_PLACE
 };
 
+/**
+ * @brief   Associates the given name and the given widget by adding them to
+ *          the list of widgets
+ */
 void ei_add_widget_to_list(char *name, ei_widget_t widget);
 
+/**
+ * @brief   Adds the given number to the list of numbers
+ */
 void ei_add_to_list_number(double value);
 
+/**
+ * @brief   Empties the list of numbers and frees the memory
+ */
 void ei_empty_list_number();
 
+/**
+ * @brief   Checks that the list of numbers has the given length
+ *          and sets the error flag to true otherwise
+ */
 void ei_verify_numbers_list_length(int length);
 
+/**
+ * @brief   Adds the given name to the list of names
+ */
 void ei_add_to_list_name(char *name);
 
+/**
+ * @brief   Empties the list of names and frees the memory
+ */
 void ei_empty_list_name();
 
+/**
+ * @brief   Checks that the list of names has the given length
+ *          and sets the error flag to true otherwise
+ */
 void ei_verify_names_list_length(int length);
 
 #endif
