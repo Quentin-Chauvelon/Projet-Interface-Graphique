2bb54d766190c370189ce20b991ab118cdf3ec27
Quentin-Chauvelon <quentin.chauvelon@gmail.com>
2024-05-14 17:52:32 +0200

    Add documentation for the parser

--- implem/headers/parser/ei_lexer.h
+++ implem/headers/parser/ei_lexer.h
@@ -11,9 +11,10 @@
 #ifndef EI_LEXER_H
 #define EI_LEXER_H
 
-extern char *characters;
-static int current_char;
+extern char *characters; // A char array holding the content of the file to parse
+static int current_char; // The current character to parse
 
+// Enum of the tokens from the lexical analysis
 typedef enum
 {
     OB,
@@ -31,6 +32,7 @@ typedef enum
     WS
 } token;
 
+// Array to associate a token with a string
 static const char *prefix[] = {
     "{",
     "}",
@@ -46,20 +48,60 @@ static const char *prefix[] = {
     "0-9 or '.'",
     "' ' or '\\t'"};
 
+/**
+ * @brief   Initialize the current character to the beggining of the characters array
+ */
 void init_current();
 
+/**
+ * @brief   Move the current character to the next one
+ */
 void update_current();
 
+/**
+ * @brief   Return the current character
+ *
+ * @return  The current character
+ */
 char get_current();
 
+/**
+ * @brief   Parse an integer token and return its value
+ *
+ * @return  The integer value of the token
+ */
 int parse_token_integer();
 
+/**
+ * @brief   Parse a real token and return its value
+ *
+ * @return  The real value of the token
+ */
 double parse_token_real();
 
+/**
+ * @brief   Parse a name token and return its value
+ *
+ * @return  The name value of the token
+ */
 char *parse_token_name();
 
+/**
+ * @brief   Check and return if the current token matches the given token
+ *
+ * @param   token   The token to check
+ *
+ * @return  True if the current token matches the given token, false otherwise
+ */
 bool is_current_token(token token);
 
+/**
+ * @brief   Parse the current token and return its value
+ *
+ * @param   token   The token to parse
+ *
+ * @return  The value of the token
+ */
 void *parse_token(token token);
 
 #endif
--- implem/headers/parser/ei_parser_option_value_configure.h
+++ implem/headers/parser/ei_parser_option_value_configure.h
@@ -14,6 +14,19 @@
 #include "../api/ei_types.h"
 #include "../implem/headers/parser/ei_parser_types.h"
 
+/**
+ * @brief   Set the value of an option for a widget (configure or place).
+ *          For example, if action is OPTION_CONFIGURE, the widget is a
+ *          toplevel, option_name is "title" and option_value is "Toplevel",
+ *          the function will call ei_toplevel_configure with only the parameter
+ *          title non NULL and equal to "Toplevel".
+ *
+ * @param action        The action to perform (configure or place)
+ * @param widget        The widget to set the option value for
+ * @param option_name   The name of the option
+ * @param option_value  The value of the option
+ * @param cast_type     The type to cast the value to if there can be multiple types
+ */
 void ei_set_option_value(enum options_action action, ei_widget_t widget, char *option_name, void *option_value, int *cast_tye);
 
 #endif
